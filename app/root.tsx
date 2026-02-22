9cc8ee2cdb20bec08336645244ec58c8b5fcd52f
alfazh123 <alfazh291@gmail.com>
2026-02-22 11:57:25 +0700

    tes

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,11 @@
 import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+	BrowserRouter,
+	isRouteErrorResponse,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
 } from "react-router";
 
 import type { Route } from "./+types/root";
@@ -25,20 +26,22 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<BrowserRouter basename={"/alfazh-porto/"}>
+				<body>
+					{children}
+					<ScrollRestoration />
+					<Scripts />
+				</body>
+			</BrowserRouter>
+		</html>
+	);
 }
 
 export default function App() {
