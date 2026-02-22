3a7a09c249d57c37a066cbb0b22494df43bcd4c4
alfazh123 <alfazh291@gmail.com>
2026-02-22 11:27:18 +0700

    update workflow

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,8 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: true,
-	async prerender() {
-		return ["/"]; // Masukkan semua path yang ingin Anda jadikan .html
-	},
+	ssr: false,
+	prerender: true,
 } satisfies Config;
