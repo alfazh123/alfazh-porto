5edbebf9fcca8e60862de1d7347ba84314eac062
alfazh123 <alfazh291@gmail.com>
2026-02-26 11:58:37 +0700

    init landing page

--- app/components/hero-index.tsx
+++ app/components/hero-index.tsx
@@ -1,97 +1,18 @@
-import { useEffect, useRef, useState } from "react";
-
 export default function HeroIndex() {
-    const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }));
-    
-      const canvasRef = useRef<HTMLCanvasElement>(null);
-    
-      useEffect(() => {
-        const interval = setInterval(() => {
-          setTime(new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }));
-        }, 60000);
-    
-        return () => clearInterval(interval);
-      }, [])
-    
-      useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-    
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
-    
-        canvas.width = canvas.offsetWidth;
-        canvas.height = canvas.offsetHeight;
-    
-        canvas.style.backgroundColor = '#f3f4f600';
-    
-        const squareSize = 20;
-        let mouseX = -1000;
-        let mouseY = -1000;
-    
-        const handleMouseMove = (e: MouseEvent) => {
-          const rect = canvas.getBoundingClientRect();
-          mouseX = e.clientX - rect.left;
-          mouseY = e.clientY - rect.top;
-        };
-    
-        const animate = () => {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-          for (let y = 0; y < canvas.height; y += squareSize) {
-            for (let x = 0; x < canvas.width; x += squareSize) {
-              if ((x / squareSize + y / squareSize) % 2 === 0) {
-                continue;
-              } else {
-                const centerX = x + squareSize / 2;
-                const centerY = y + squareSize / 2;
-                const distance = Math.hypot(centerX - mouseX, centerY - mouseY);
-                const repelDistance = 100;
-                const radius = squareSize / 4;
-    
-                let offsetX = 0;
-                let offsetY = 0;
-                if (distance < repelDistance && distance > 0) {
-                  const angle = Math.atan2(centerY - mouseY, centerX - mouseX);
-                  offsetX = Math.cos(angle) * (repelDistance - distance) * 0.3;
-                  offsetY = Math.sin(angle) * (repelDistance - distance) * 0.3;
-                }
-    
-                ctx.fillStyle = '#d1d5db';
-                ctx.beginPath();
-                ctx.arc(centerX + offsetX, centerY + offsetY, radius, 0, Math.PI * 2);
-                ctx.fill();
-              }
-            }
-          }
-          requestAnimationFrame(animate);
-        };
-    
-        canvas.addEventListener('mousemove', handleMouseMove);
-        animate();
-    
-        return () => {
-          canvas.removeEventListener('mousemove', handleMouseMove);
-        };
-      }, []);
-
-    return (
-			<div className={`h-screen relative gap-4 overflow-clip`}>
-				<div className="h-full flex flex-col justify-center py-8">
-					<div className="flex flex-col z-50 bg-slate-50 w-fit">
-						<div className="text-8xl font-bold">Ahmd Alfazh</div>
-						<div>
-							I'm a FrontEnd Developer Experience in React, NextJs, and Tailwind
-							CSS.
-						</div>
-					</div>
-				</div>
-				<div>
-					<canvas
-						ref={canvasRef}
-						className=" absolute top-0 left-0 w-full h-full mask-b-from-0% mask-r-from-10%"
-					/>
+	return (
+		<div className="relative h-[60vh] max-w-4xl mx-auto md:px-12 px-8 flex flex-col justify-center py-8 mb-20">
+			<div className="flex flex-col z-50">
+				<div className="md:text-8xl text-6xl font-bold w-fit">Ahmd Alfazh</div>
+				<div className="lg:w-fit md:w-3/4 w-full">
+					I'm a FrontEnd Developer Experience in React, NextJs, and Tailwind
+					CSS.
 				</div>
 			</div>
-		);
+			<img
+				src="/plant.svg"
+				alt="Ahmd Alfazh"
+				className="absolute -bottom-10 md:-right-10 -right-32 w-80 object-cover mx-auto mt-4 bg-clip-text"
+			/>
+		</div>
+	);
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,9 @@
 import { ArrowUpRight } from "lucide-react";
 import type { Route } from "./+types/home";
 import HeroIndex from "~/components/hero-index";
-import { VscGithubAlt } from "react-icons/vsc";
-import { SiLinkedin } from "react-icons/si";
-import { GoMail } from "react-icons/go";
+import WriteCard from "~/components/write-card";
+import ProjectCard from "~/components/project-card";
+import { projects, writes } from "~/utils/data";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -13,136 +13,30 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-	const projects = [
-		{
-			name: "Image Editor",
-			description:
-				"Image editor web app, built with React and Tailwind CSS. implementation of `rust-editor` package",
-			link: "https://github.com/alfazh123/image-editor",
-			year: 2025,
-		},
-		{
-			name: "PathXplorer",
-			description:
-				"Mobile app for exploring interest and talent of high school students, built with Kotlin.",
-			link: "https://github.com/PathXplorer-C242-PS289",
-			year: 2024,
-		},
-	];
-
-	const writes = [
-		{
-			title: "Build WASM package with Rust",
-			year: 2025,
-			topics: ["WASM", "Rust", "Web Development"],
-			link: "https://medium.com/@alfazh291/built-wasm-package-with-rust-99c3c76f273e",
-		},
-	];
-
-	const contacts = [
-		{
-			type: "contacts/mail.png",
-			value: "mailto:alfazh.work@gmail.com",
-		},
-		{
-			type: "contacts/linkedin.png",
-			value: "https://www.linkedin.com/in/ahmd-mufahras-li-alfazh-assardew/",
-		},
-		{
-			type: "contacts/github.png",
-			value: "https://github.com/alfazh123",
-		},
-	];
-
 	return (
-		<div className="home px-4">
-			{/* <HeroIndex /> */}
-			<div className="h-[50vh] max-w-4xl mx-auto px-12 flex flex-col justify-center py-8">
-				<div className="flex flex-col z-50 bg-slate-50 w-fit">
-					<div className="text-8xl font-bold">Ahmd Alfazh</div>
-					<div>
-						I'm a FrontEnd Developer Experience in React, NextJs, and Tailwind
-						CSS.
-					</div>
-				</div>
-			</div>
+		<div className="home px-4 overflow-clip">
+			<HeroIndex />
 
-			<div className="md:max-w-4xl mx-auto p-8 grid md:grid-cols-2 grid-cols-1 rounded-lg justify-center">
-				<div className="rounded-lg w-full justify-center mb-8 h-80">
+			<div className="md:max-w-4xl mx-auto md:p-8 p-4 grid md:grid-cols-2 grid-cols-1 rounded-lg justify-center gap-4">
+				<div className="rounded-lg w-full justify-center mb-8 md:h-80 h-fit">
 					<h2 className="text-6xl font-bold mb-4">Selected Project</h2>
 				</div>
 				{projects.map((project, index) => (
-					<a
-						key={index}
-						href={project.link}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="border-b-2 border-gray-300 p-4 flex flex-col justify-between h-80 hover:bg-slate-100 hover:scale-110 transition-transform duration-300 rounded-t-lg">
-						<div>
-							<h3 className="text-2xl font-semibold">{project.name}</h3>
-							<p className="text-gray-600">{project.description}</p>
-						</div>
-						<div className="w-full flex justify-end">
-							<p className="text-6xl">{project.year}</p>
-							<ArrowUpRight className="w-12 h-12" />
-						</div>
-					</a>
+					<ProjectCard {...project} key={index} />
 				))}
 			</div>
 
-			<div className="md:max-w-4xl mx-auto p-8 grid md:grid-cols-2 grid-cols-1 rounded-lg justify-center mt-16">
-				<div className="rounded-lg w-full justify-center mb-8 h-60">
-					<h2 className="text-6xl font-bold mb-4">Selected Writes</h2>
+			<div className="md:max-w-4xl mx-auto md:px-8 px-4 grid md:grid-cols-2 grid-cols-1 rounded-lg justify-center py-20">
+				<div className="rounded-lg w-full justify-center mb-8 md:h-80 h-fit">
+					<h2 className="text-6xl font-bold md:mb-4 mb-8">Selected Writes</h2>
 				</div>
 				{writes.map((write, index) => (
-					<a
+					<div
 						key={index}
-						href={write.link}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="group border-b border-gray-300 p-4 rounded-lg flex justify-center items-center text-center rotate-6 bg-slate-100 hover:scale-110 transition-transform duration-300">
-						<div className="gochi-hand-regular">
-							<h3 className="text-4xl group-hover:underline">{write.title}</h3>
-							<div className="flex gap-2 mx-auto justify-center">
-								{write.topics.map((topic, id) => (
-									<div
-										key={id}
-										className="text-gray-600 bg-slate-100 px-2 py-1 rounded text-sm">
-										{topic}
-									</div>
-								))}
-							</div>
-						</div>
-						<div>
-							{/* <ArrowUpRight className="w-8 h-8" /> */}
-							{/* <p className="text-6xl">{write.year}</p> */}
-						</div>
-					</a>
+						className={`${index % 2 === 0 ? "md:justify-self-start" : "md:justify-self-end"} flex justify-center`}>
+						<WriteCard {...write} />
+					</div>
 				))}
-			</div>
-
-			{/* Contacs */}
-			<div className="md:max-w-4xl mx-auto p-8 flex flex-col rounded-lg h-[50vh] justify-center">
-				<div className="rounded-lg w-full justify-center text-center mb-4">
-					<h2 className="text-6xl font-bold">Contacts</h2>
-					<p className="text-lg">
-						If you want to get in touch 👋, feel free to reach out to me on link
-						below.
-					</p>
-				</div>
-				<div className="flex gap-8 justify-center">
-					{contacts.map((contact, index) => (
-						<div key={index} className="flex justify-center items-center">
-							<a href={contact.value} target="_blank" rel="noopener noreferrer">
-								<img
-									src={contact.type}
-									alt={`Contact ${index + 1}`}
-									className="w-12 h-12"
-								/>
-							</a>
-						</div>
-					))}
-				</div>
 			</div>
 		</div>
 	);
